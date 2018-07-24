8712e86f94afd0a1347de54e410ba5aad427b5f8
rldan <rldan@thoughtworks.com>
2018-07-24 19:03:54 +0800

    add lombok for @Slf4j and add log for exception

---
+++ with-spring-boot/src/main/java/net/shadow/withspring/exception/RemoteInvokeException.java
@@ -0,0 +1,34 @@
+package net.shadow.withspring.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class RemoteInvokeException extends RuntimeException {
+    private HttpStatus httpStatus;
+    private String errorCode;
+    private String message;
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public void setHttpStatus(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- with-spring-boot/src/main/java/net/shadow/withspring/feign/ClientErrorDecoder.java
+++ with-spring-boot/src/main/java/net/shadow/withspring/feign/ClientErrorDecoder.java
@@ -1,12 +1,30 @@
 package net.shadow.withspring.feign;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ClientErrorDecoder extends ErrorDecoder.Default {
+
+    private ObjectMapper objectMapper = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     @Override
     public Exception decode(String methodKey, Response response) {
         return super.decode(methodKey, response);
+
+//        try {
+//            String body = CharStreams.toString(response.body().asReader());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        ErrorResult result = objectMapper.readValue(body, ErrorResult.class);
+//
+//        return new ServiceRequestException(HttpStatus.valueOf(response.status()),
+//                ErrorCode.fromValue(result.getErrorCode()),
+//                result.getMessage());
     }
 }
